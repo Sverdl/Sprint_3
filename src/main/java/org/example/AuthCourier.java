b2b7134f5440802e2dcfe01abfa992fa31fe9140
Evgeny Tikhonov <evgeny.d.tikhonov@yandex.ru>
2022-04-10 14:13:53 +0300

    Sprint_3 for review

---
+++ src/main/java/org/example/AuthCourier.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+
+public class AuthCourier {
+
+
+    public final String login;
+    public final String password;
+    public int id;
+
+    public AuthCourier(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
